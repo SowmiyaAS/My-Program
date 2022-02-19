2861a4f540d138abe5842b952a1dd6fa950f9088
SowmiyaAS <99643073+SowmiyaAS@users.noreply.github.com>
2022-02-19 20:37:58 +0530

    Add files via upload

---
+++ Breakstatement.java
@@ -0,0 +1,23 @@
+package Jumpstatement;
+
+public class Breakstatement {
+
+	public static void main(String[] args) {
+		//reverse print
+		//for(int i=20; i>=1;  i--)
+		
+		for (int i = 0; i<=20; i++)
+			
+			{
+			if(i==10)
+			{
+				System.out.println("Ten");
+				break;
+				
+			}
+			System.out.println(i);
+
+	}
+
+}
+}
---
+++ Continuestatement.java
@@ -0,0 +1,22 @@
+package Jumpstatement;
+
+public class Continuestatement {
+
+	public static void main(String[] args) {
+		//for (int i = 0; i<=10 ; i++) 
+		
+			for(int i=10 ; i>=1; i--)
+			
+		{
+			if(i==7)
+			{
+				System.out.println("seven");
+				continue;
+				
+			}
+			System.out.println(i);
+
+	}
+	}
+	
+}
