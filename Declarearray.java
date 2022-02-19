be666057e3ad6096075186ab967d3276ddc0f02e
SowmiyaAS <99643073+SowmiyaAS@users.noreply.github.com>
2022-02-19 20:36:40 +0530

    Add files via upload

---
+++ Declarearray.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class Declarearray {
+
+	public static void main(String[] args) {
+		//declare array
+		//array length 20
+		//index value 19
+		//index value starting 0
+		int[] array= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.println(array[i]);
+		     }
+		
+		//for (int i= array.length-1 ; i>=0; i--) print reverse array
+
+
+	}
+
+}
